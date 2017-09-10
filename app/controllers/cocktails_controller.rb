0658d6226d66f5078f7c0f28367547837eb68e38
Trystan Newbury trystan.newbury@gmail.com <trystan.newbury@gmail.com>
2017-09-10 23:15:02 +0200

    Fix photo uploader

--- app/controllers/cocktails_controller.rb
+++ app/controllers/cocktails_controller.rb
@@ -30,6 +30,6 @@ class CocktailsController < ApplicationController
   end
 
   def params_cocktail
-    params.require(:cocktail).permit(:name)
+    params.require(:cocktail).permit(:name,:photo,:photo_cache)
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,7 @@
 Rails.application.routes.draw do
-  root 'cocktails#index'
+
+  root to: 'cocktails#index'
+
   resources :cocktails, only: [:index,:show,:new,:create] do
     resources :doses, only: [:new,:create]
   end
